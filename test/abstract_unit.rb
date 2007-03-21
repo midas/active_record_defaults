46fd098d712c126fab83e33c017ffd8c55e642d4
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2007-03-21 00:16:04 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/active_record_defaults@224 20afb1e0-9c0e-0410-9884-91ed27886737

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -20,9 +20,9 @@ require 'active_record/fixtures'
 
 require File.dirname(__FILE__) + '/../lib/active_record_defaults'
 
-config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
+ActiveRecord::Base.configurations = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
-ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'mysql'])
+ActiveRecord::Base.establish_connection(ENV['DB'] || 'mysql')
 
 load(File.dirname(__FILE__) + '/schema.rb')
 
