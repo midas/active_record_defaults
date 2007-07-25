1bedb603161ff2e712f97f9e40fd30d2f43db6ce
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2007-07-25 05:49:39 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/active_record_defaults@273 20afb1e0-9c0e-0410-9884-91ed27886737

--- test/fixtures/person.rb
+++ test/fixtures/person.rb
@@ -7,14 +7,18 @@ class Person < ActiveRecord::Base
     'Fitzpatrick'
   end
   
-  defaults :lucky_number => lambda { 2 }
+  defaults :lucky_number => lambda { 2 }, :favourite_colour => :default_favourite_colour
   
   def defaults
     self.birthdate = Date.new(2006, 10, lucky_number) if lucky_number?
   end
   
+  def default_favourite_colour
+    "Blue"
+  end
+  
   # Include an aggregate reflection to check compatibility
-  composed_of :address, :mapping => [%w{address_suburb suburb}, %{address_city city}]
+  composed_of :address, :mapping => [%w(address_suburb suburb), %{address_city city}]
   
   belongs_to :school
 end
